9ef247f2c4609dc80b8031696a6c0138994c085d
txu1 <txu1@i9i8.com>
2020-04-30 14:09:50 +0800

    init project

---
+++ src/main/java/com/rabbitears/RabbitearsApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitearsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitearsApplication.class, args);
+    }
+
+}
